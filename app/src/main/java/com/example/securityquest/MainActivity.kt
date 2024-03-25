02624000a60a3c958958f1f527172aa9f252488f
mohawkatoms <mohawkatoms@gmail.com>
2024-03-25 17:57:36 +0100

    add app icon and starting Page

--- app/src/main/java/com/example/securityquest/MainActivity.kt
+++ app/src/main/java/com/example/securityquest/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.example.securityquest
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.securityquest.ui.theme.SecurityQuestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             SecurityQuestTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +58,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StartingPage()
                 }
             }
         }
@@ -30,17 +66,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun StartingPage(modifier: Modifier = Modifier) {
+    Box (modifier){
+        Text(
+            text = stringResource(id = R.string.background_binaries),
+            modifier = Modifier.alpha(0.25F),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp,
+            )
+        )
+        Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(140.dp)){
+            Text(
+                text = "Security Quest",
+                fontWeight = FontWeight.Bold,
+                fontSize = 80.sp,
+                lineHeight = 80.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 130.dp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SecurityQuestTheme {
-        Greeting("Android")
+            )
+            ExtendedFloatingActionButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(Icons.Filled.Lock, "Lock Icon", modifier = Modifier.padding(end = 12.dp))
+                Text(
+                    text = "Start", style = TextStyle(
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+        }
     }
 }
+
