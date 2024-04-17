bb6064589c387f151dc47f0e3d2d464e2002be2a
mohawkatoms <mohawkatoms@gmail.com>
2024-04-17 12:57:48 +0200

    refactor variables

--- app/src/main/java/com/example/securityquest/MainActivity.kt
+++ app/src/main/java/com/example/securityquest/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.List
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material.icons.rounded.Star
@@ -27,6 +28,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FilledIconToggleButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.InputChip
@@ -48,8 +50,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ClipboardManager
-import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -58,11 +58,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.securityquest.ui.theme.SecurityQuestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
 
         setContent {
             SecurityQuestTheme {
@@ -116,22 +118,27 @@ fun StartingPage(modifier: Modifier = Modifier) {
         var isPasswordDialogOpen by rememberSaveable {
             mutableStateOf(false)
         }
+        var isExplanationDialogOpen by rememberSaveable {
+            mutableStateOf(false)
+        }
         var passwordFromUser by rememberSaveable {
             mutableStateOf("")
         }
         Row(horizontalArrangement = Arrangement.End) {
-            FilledIconButton(
-                onClick = { isPasswordDialogOpen = true },
+            FilledIconToggleButton(
+                checked = isPasswordDialogOpen,
+                onCheckedChange = { isPasswordDialogOpen = true },
                 modifier = Modifier.padding(start = 10.dp, top = 10.dp)
             ) {
                 Icon(imageVector = Icons.Rounded.Star, contentDescription = "Create Password")
             }
             Spacer(Modifier.weight(1f))
-            FilledIconButton(
-                onClick = { /*TODO*/ },
+            FilledIconToggleButton(
+                checked = isExplanationDialogOpen,
+                onCheckedChange = { isExplanationDialogOpen = true },
                 modifier = Modifier.padding(end = 10.dp, top = 10.dp)
             ) {
-                Icon(imageVector = Icons.Rounded.Settings, contentDescription = "Settings")
+                Icon(imageVector = Icons.Rounded.List, contentDescription = "Explanation")
             }
         }
         Column(
@@ -364,6 +371,77 @@ fun StartingPage(modifier: Modifier = Modifier) {
                             passwordFromUser = generatedPassword
                         }) {
                             Text(text = "Einsetzen")
+                        }
+                    }
+                }
+            }
+
+        }
+        if (isExplanationDialogOpen) {
+            Dialog(onDismissRequest = { isExplanationDialogOpen = false }) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(500.dp)
+                        .padding(16.dp), shape = RoundedCornerShape(16.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant
+                    ),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 6.dp
+                    )
+                ) {
+                    Text(
+                        text = "Passwort erstellen",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(13.dp)
+                    )
+                    Text(
+                        text = "Das ist eine Anleitung zur Erstellung sicherer Passwörter",
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(start = 13.dp)
+                    )
+                    Text(
+                        text = "Länge",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(start = 13.dp, top = 10.dp)
+                    )
+                    Text(
+                        text = "Je länger das Passwort, desto schwieriger ist es zu knacken. Empfohlen werden mindestens 12 Zeichen.",
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(start = 13.dp, end = 13.dp),
+                        lineHeight = 19.sp
+                    )
+                    Text(
+                        text = "Komplexität",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(start = 13.dp, top = 10.dp)
+                    )
+                    Text(
+                        text = "Ein gutes Passwort enthält eine Mischung aus Groß- und Kleinbuchstaben, Zahlen, Sonderzeichen und gegebenenfalls auch Leerzeichen.",
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(start = 13.dp, end = 13.dp),
+                        lineHeight = 19.sp
+                    )
+                    Text(
+                        text = "Einzigartigkeit",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(start = 13.dp, top = 10.dp)
+                    )
+                    Text(
+                        text = "Verwenden Sie niemals dasselbe Passwort für mehrere Konten oder Dienste.",
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(start = 13.dp, end = 13.dp),
+                        lineHeight = 19.sp
+                    )
+                    Row {
+                        Spacer(Modifier.weight(1f))
+                        TextButton(onClick = { isExplanationDialogOpen = false }, modifier = Modifier.padding(top = 10.dp)) {
+                            Text(text = "Schließen")
                         }
                     }
                 }
